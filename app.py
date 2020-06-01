bba0d24f68aa626e10a34e92e28d9a7e91895c35
Ham-mi <churia2091@gmail.com>
2020-06-02 03:59:20 +0900

    '2020-06-02'

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from flask import Flask,request,render_template, redirect, url_for
+import game,json
+
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+    return '메인페이지'
+
+@app.route('/hello/')
 def hello():
     return 'Hello,World!'
-@app.route('/hello') 
+
+@app.route('/form') 
 def hellohtml(): 
     return render_template("hello.html") 
+
 @app.route('/method', methods=['GET', 'POST']) 
 def method(): 
     if request.method == 'GET': 
@@ -18,7 +26,16 @@ def method():
     else: 
         num = request.form["num"] 
         name = request.form["name"] 
+        with open("static/save.txt","w", encoding='utf-8') as f: 
+            f.write("%s,%s" % (num, name))
         return "POST로 전달된 데이터({}, {})".format(num, name) 
+
+@app.route('/getinfo') 
+def getinfo(): # 파일 입력 
+    with open("static/save.txt", "r", encoding='utf-8') as file: 
+        student = file.read().split(',')  # 쉽표로 잘라서 student 에 배열로 저장 
+    return '번호 : {}, 이름 : {}'.format(student[0], student[1])
+
 @app.route('/login')
 def login():
     return render_template("login.html")
@@ -46,5 +63,46 @@ def moveNaver():
 @app.route('/move/daum')
 def moveDaum():
     return redirect(url_for('daum'))
+
+@app.route('/hello/<name>')
+def hellovar(name):
+    character = game.set_charact(name)
+    return render_template('gamestart.html', data=character)
+
+@app.route('/input/<int:num>')
+def input_num(num):
+    if num ==1:
+        with open("static/save.txt", "r", encoding='utf-8') as f: 
+            data = f.read() 
+            character = json.loads(data) 
+            print(character['items'])
+        return "{}이 {} 아이템을 사용해서 이겼다.".format(character["name"],character["items"][0])
+    elif num ==2:
+        return "도망가다!"
+    elif num ==3:
+        return "3번친구"
+    else :
+        return "아앗, 없어요"
+
+@app.route('/gamestart')
+def gamestart():
+    with open("static/save.txt", "r", encoding='utf-8') as f: 
+        data = f.read() 
+        character = json.loads(data) 
+        print(character['items'])
+    return "{}이 {} 아이템을 사용해서 이겼다.".format(character["name"],character["items"][0])
+
+@app.route('/stage2')
+def stage2():
+    with open("static/save.txt", "r", encoding='utf-8') as f: 
+        data = f.read() 
+        character = json.loads(data) 
+    return render_template('view.html', items=character['items'])
+    
+@app.route('/senddata') 
+def senddata(): 
+    name = 'world' 
+    return render_template('senddata.html', data=name)
+
 if __name__ == '__main__': 
     app.run(debug=True)
